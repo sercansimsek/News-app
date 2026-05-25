a0f237bbf8654f07e8d21a4dcc173a9925072973
Sercan Şimşek <sercansimsek44@gmail.com>
2026-05-25 15:13:30 +0200

    displayNews function added

--- scripts.js
+++ scripts.js
@@ -7,8 +7,31 @@ async function fetchNews() {
     const data = await response.json();
     console.log(data);
     // TODO: Add a function call to display the news
+    displayNews(data.articles);
   } catch (error) {
     console.error("There was an error!", error);
+  }
+}
+
+function displayNews(articles) {
+  const newsDiv = document.querySelector("#news");
+  for (const article of articles) {
+    const articleDiv = document.createElement("div");
+
+    //create and append a headline to the articleDiv
+    const title = document.createElement("h4");
+    title.textContent = article.title;
+    articleDiv.appendChild(title);
+
+    // TODO: Use document.createElement and appendChild to create and append more elements
+    const description = document.createElement("p");
+    description.textContent = article.description;
+    articleDiv.appendChild(description);
+
+    const author = document.createElement("p");
+    author.textContent = article.author;
+    articleDiv.appendChild(author);
+    newsDiv.appendChild(articleDiv);
   }
 }
 
